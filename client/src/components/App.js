77dfeb11ede36070dad85ea744faf4dda47bb62e
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2023-03-11 18:55:51 +0100

    Updated App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // Import React package
 import React from "react";
 
@@ -14,7 +16,7 @@ const CONTRACT_ADDRESS = require("../contracts/SimpleStorage.json").networks[5].
 const CONTRACT_ABI = require("../contracts/SimpleStorage.json").abi;
 
 export default class App extends React.Component {
-  state = { web3Provider: null, accounts: null, contract: null, storageValue: null };
+  state = { web3Provider: null, accounts: null, networkId: null, contract: null, storageValue: null };
 
   componentDidMount = async () => {
     try {
@@ -35,7 +37,7 @@ export default class App extends React.Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3Provider: web3, accounts: accounts, contract: contract, storageValue: response });
+      this.setState({ web3Provider: web3, accounts: accounts, networkId: networkId, contract: contract, storageValue: response });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -64,21 +66,31 @@ export default class App extends React.Component {
 
   render() {
     if (!this.state.web3Provider) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div className="App-no-web3">
+        <h3>No Web3 connection... 🧐</h3>
+        <p>Jump to the next chapter to configure the Web3 Provider.</p>
+        <h3>Let's go! ⏭️</h3>
+      </div>;
     }
     return (
       <div className="App">
+        {/* DApp Header */}
+        <h1>Ready to go! 🚀</h1>
+        <p> Your Truffle Box has been succesfully installed ⚙️ </p>
+        <p> The default Smart Contract has been compiled and deployed on testnet 🧪 </p>
+        <br />
 
-        {/* Information */}
+        {/* DApp Information */}
         <h2>Smart Contract: SimpleStorage.sol 🧮</h2>
+        <p>Contract address: {CONTRACT_ADDRESS}</p>
         <p>
           If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
+          a stored value of 1 (by default).
         </p>
         <h3>Contract stored value: {this.state.storageValue} </h3>
         <br />
 
-        {/*  Actions  */}
+        {/*  DApp Actions  */}
         <p>
           Try clicking the button below 👇 to set the value on Smart Contract to 5
         </p>
