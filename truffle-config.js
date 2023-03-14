adff5a553d670ca75d4c676983462bd3a2556693
Diegoescalonaro <diegoescalonarodriguez@gmail.com>
2023-03-14 21:43:52 +0100

    Updated truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -19,9 +19,9 @@
  */
 
 // require('dotenv').config();
+// const ETHERSCAN_API_KEY = process.env["ETHERSCAN_API_KEY"]
 // const mnemonic = process.env["MNEMONIC"];
 // const infuraProjectId = process.env["INFURA_PROJECT_ID"];
-
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
@@ -43,17 +43,24 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
+    // Default network
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    // An additional network, but with some advanced options…
-    dashboard: {
+    // Ganache network
+    ganache: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 24012,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    // Dashboard network
+    // dashboard: {
+    //   host: "127.0.0.1",     // Localhost (default: none)
+    //   port: 24012,            // Standard Ethereum port (default: none)
+    //   network_id: "*",       // Any network (default: none)
+    // },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -125,4 +132,9 @@ module.exports = {
   //     }
   //   }
   // }
+  // ----------------------------------------------
+  // **************** Plugins *********************
+  // ----------------------------------------------
+  // plugins: ['truffle-plugin-verify'],
+  // api_keys: { etherscan: ETHERSCAN_API_KEY },
 };
